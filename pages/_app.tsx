09c8b93aaf706f32c80dbccedfe23bd9fb3f809c
lastforever457 <lastforever457@gmail.com>
2024-09-24 18:51:29 +0500

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,13 @@ import LayoutComponent from "@/components/layout-component";
 import "../components/global.css";
 import "../components/swiper.css";
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({
+    Component,
+    pageProps,
+}: {
+    Component: any;
+    pageProps: any;
+}) {
     return (
         <LayoutComponent>
             <Component {...pageProps} />
