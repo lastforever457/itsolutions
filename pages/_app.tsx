ec7149f04aa23098f45d96633312f99c5508af61
lastforever457 <lastforever457@gmail.com>
2024-09-24 19:34:39 +0500

    fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,15 @@
 import LayoutComponent from "@/components/layout-component";
 import { observer } from "mobx-react-lite";
+import { AppProps } from "next/app";
 import "../components/global.css";
 import "../components/swiper.css";
 
-const MyApp = observer(
-    ({ Component, pageProps }: { Component: any; pageProps: any }) => {
-        return (
-            <LayoutComponent>
-                <Component {...pageProps} />
-            </LayoutComponent>
-        );
-    }
-);
+const MyApp = observer(({ Component, pageProps }: AppProps) => {
+    return (
+        <LayoutComponent>
+            <Component {...pageProps} />
+        </LayoutComponent>
+    );
+});
 
 export default MyApp;
